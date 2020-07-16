273f4413bc605c8eca48b63d211cebc76e1f609b
Elisa Ferracane <elisa@ferracane.com>
2020-07-16 05:00:26 -0500

    update grid search

--- code/shell_scripts/run_grid_search_electra.sh
+++ code/shell_scripts/run_grid_search_electra.sh
@@ -2,9 +2,9 @@
 for lr in 1e-5 2e-5 3e-5
 do
     echo ${lr}
-    for warmup in 0.001 0.01 0.1
+    for warmup in 0 0.001 0.01 0.1
     do
-        for decay in 0.001 0.01 0.1
+        for decay in 0 0.001 0.01 0.1
         do
             python -u -m models.bert --dataset CongressionalHearing --model-family electra --model google/electra-base-discriminator --max-seq-length 512 --evaluate-dev --patience 5 --lr ${lr} --warmup-proportion ${warmup} --weight-decay ${decay} --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_electra_pat5_lr${lr}_warmup${warmup}_decay${decay}_1.json > ch_electra_pat5_lr${lr}_warmup${warmup}_decay${decay}_1.log 2>&1
             python -u -m models.bert --dataset CongressionalHearing --model-family electra --model google/electra-base-discriminator --max-seq-length 512 --evaluate-dev --patience 5 --lr ${lr} --warmup-proportion ${warmup} --weight-decay ${decay} --batch-size 8 --epochs 30 --seed 5678 --metrics-json metrics_electra_pat5_lr${lr}_warmup${warmup}_decay${decay}_2.json > ch_electra_pat5_lr${lr}_warmup${warmup}_decay${decay}_2.log 2>&1
--- code/shell_scripts/run_grid_search_roberta.sh
+++ code/shell_scripts/run_grid_search_roberta.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-for lr in 2e-5 3e-5
+for lr in 1e-5 2e-5 3e-5
 do
     echo ${lr}
-    for warmup in 0
+    for warmup in 0 0.001 0.01 0.1
     do
-        for decay in 0.001 0.01 0.1
+        for decay in 0 0.001 0.01 0.1
         do
-            for batch_size in 8 16 32
+            for batch_size in 8
             do
             python -u -m models.bert --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-dev --patience 5 --lr ${lr} --warmup-proportion ${warmup} --weight-decay ${decay} --batch-size ${batch_size} --epochs 30 --seed 1234 --metrics-json metrics_roberta_pat5_lr${lr}_warmup${warmup}_decay${decay}_batch${batch_size}_1.json > ch_roberta_pat5_lr${lr}_warmup${warmup}_decay${decay}_batch${batch_size}_1.log 2>&1
             python -u -m models.bert --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-dev --patience 5 --lr ${lr} --warmup-proportion ${warmup} --weight-decay ${decay} --batch-size ${batch_size} --epochs 30 --seed 5678 --metrics-json metrics_roberta_pat5_lr${lr}_warmup${warmup}_decay${decay}_batch${batch_size}_2.json > ch_roberta_pat5_lr${lr}_warmup${warmup}_decay${decay}_batch${batch_size}_2.log 2>&1
