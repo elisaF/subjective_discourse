4234aef6f1a2addb45f4add7e81b6c292ccd1bfc
Elisa Ferracane <elisa@ferracane.com>
2020-07-19 09:53:43 -0500

    update shell script

--- code/shell_scripts/run_roberta_hierarchical_3.sh
+++ code/shell_scripts/run_roberta_hierarchical_3.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-expnames=(gold_sentiments_coarse_num_r_text_q_text_last_question gold_sentiments_num_r_text_q_text_last_question)
+expnames=(r_text_q_text_last_question_gold_sentiments_num r_text_q_text_last_question_gold_sentiments_coarse_num)
 declare -A column=(
-  [r_text_q_text_last_question]=4
-  [gold_sentiments_coarse_num_r_text_q_text_last_question]=16
-  [gold_sentiments_num_r_text_q_text_last_question]=14
+  [r_text_q_text_last_question_gold_sentiments_num]=14
+  [r_text_q_text_last_question_gold_sentiments_coarse_num]=16
 )
 
 for expname in "${expnames[@]}"; do
   echo $expname
-  python -u -m models.bert_hier.main_cv --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-test --patience 30 --lr 3e-5 --warmup-proportion 0.1 --weight-decay 0.1 --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_roberta_hierarchical_${expname}_test.json --second-input-column 2 --use-second-input --third-input-column 4 --use-third-input --first-input-column ${column[$expname]}  > ch_roberta_hierarchical_${expname}_test.log 2>&1
+  python -u -m models.bert_hier.main_cv --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-test --patience 30 --lr 1e-5 --warmup-proportion 0.1 --weight-decay 0.001 --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_roberta_hierarchical_${expname}_test.json --first-input-column 2 --use-second-input --second-input-column 4 --use-third-input --third-input-column ${column[$expname]}  > ch_roberta_hierarchical_${expname}_test.log 2>&1
 done
 
 
