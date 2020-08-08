98a6c4b54e87e8bdfdb20e020e3e5805a879f48c
Elisa Ferracane <elisa@ferracane.com>
2020-08-08 16:25:08 -0500

    update shell script params

--- code/shell_scripts/run_roberta_classification_4.sh
+++ code/shell_scripts/run_roberta_classification_4.sh
@@ -9,7 +9,7 @@ declare -A column=(
 
 for expname in "${expnames[@]}"; do
   echo $expname
-  python -u -m models.bert.main_cv --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-test --patience 5 --lr 1e-5 --warmup-proportion 0.001 --weight-decay 0 --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_roberta_classification_${expname}_test.json --second-input-column 2 --use-second-input --first-input-column 4 --use-third-input --third-input-column 10 --use-fourth-input --fourth-input-column ${column[$expname]} > ch_roberta_classification_${expname}_test.log 2>&1
+  python -u -m models.bert.main_cv --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-test --patience 5 --lr 1e-5 --warmup-proportion 0.01 --weight-decay 0 --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_roberta_classification_${expname}_test.json --second-input-column 2 --use-second-input --first-input-column 4 --use-third-input --third-input-column 10 --use-fourth-input --fourth-input-column ${column[$expname]} > ch_roberta_classification_${expname}_test.log 2>&1
 done
 
 
--- code/shell_scripts/run_roberta_hierarchical_round_robin_3.sh
+++ code/shell_scripts/run_roberta_hierarchical_round_robin_3.sh
@@ -13,7 +13,7 @@ for first in "${expnames[@]}"; do
          for third in  "${expnames[@]}"; do
                if [[ "$first" != "$second" ]] && [[ "$second" != "$third" ]] && [[ "$first" != "$third" ]]; then
                    echo $first $second $third
-                   python -u -m models.bert_hier.main_cv --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-test --patience 30 --lr 1e-5 --warmup-proportion 0.001 --weight-decay 0 --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_roberta_hierarchical_${first}_${second}_${third}_test.json --first-input-column ${column[$first]} --use-second-input --second-input-column ${column[$second]} --use-third-input --third-input-column ${column[$third]} > ch_roberta_hierarchical_${first}_${second}_${third}_test.log 2>&1
+                   python -u -m models.bert_hier.main_cv --dataset CongressionalHearing --model-family roberta --model roberta-base --max-seq-length 512 --evaluate-test --patience 30 --lr 1e-5 --warmup-proportion 0.01 --weight-decay 0 --batch-size 8 --epochs 30 --seed 1234 --metrics-json metrics_roberta_hierarchical_${first}_${second}_${third}_test.json --first-input-column ${column[$first]} --use-second-input --second-input-column ${column[$second]} --use-third-input --third-input-column ${column[$third]} > ch_roberta_hierarchical_${first}_${second}_${third}_test.log 2>&1
                fi
          done
      done
